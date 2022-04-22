92884300c67d80f1f9c2d284afbba471e0d590f6
sergio flores <junie33@gmail.com>
2022-04-22 16:28:36 -0700

    Added class constructor

--- Veg-checker.js
+++ Veg-checker.js
@@ -9,10 +9,27 @@ function getFetch (){
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            if(data.status === 1){
+                const item = new ProductInfo(data.product)
+                item.testCase()     
+            }else if(data.status === 0){
+                alert(`Product ${inputVal} not found. Please enter a correct barcode`)
+            }
             document.querySelector('#product-img').src = data.product.image_url
-            document.querySelector('#product-name').innerText = data.product.product_name
+            document.querySelector('#product-name').innerText = data.product.product_name 
         })
         .catch(err =>{
         console.log(`error ${err}`)
     })
+}
+
+class ProductInfo{
+    constructor(productData){
+        this.name = productData.product_name
+        this.ingredient = productData.ingredients
+        this.image = productData.image_url
+    }
+    testCase(){
+        console.log(this.ingredient)
+    }
 }
